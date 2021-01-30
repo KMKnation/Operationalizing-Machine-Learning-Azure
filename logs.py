f0745efa472b255099f7016d4c1efc9dbf0a1321
unknown <kanojiyamayur@gmail.com>
2021-01-30 15:34:42 +0000

    feat: upto swagger documentation

--- logs.py
+++ logs.py
@@ -5,7 +5,7 @@ from azureml.core.webservice import Webservice
 ws = Workspace.from_config()
 
 # Set with the deployment name
-name = "project-two-automl-deployment"
+name = "project-two-bestmodel-deployment"
 
 # load existing web service
 service = Webservice(name=name, workspace=ws)
